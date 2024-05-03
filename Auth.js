a89bb84a238793b61f5a49d78e1003ce86dca2fe
Kamal <154740582+KAMAL-02@users.noreply.github.com>
2024-05-03 21:57:57 +0530

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,59 @@
+//For appwrite but can be modified to work for any Backend service
+import conf from '../conf.js'
+import {Client , Account , ID} from 'appwrite'
+
+export class AuthService{
+    client = new Client();
+    account; //Account is a property here
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId); 
+
+        this.account = new Account(this.client);
+    }
+    
+    async createAccount({email , password , name}){
+        try {
+            const  userAccount = await this.account.create(ID.unique() , email , password , name);
+            if(userAccount){
+                //call another method which will login directly
+                return this.login({email , password})
+            }else{
+                return userAccount;
+            }
+        } catch (error) {
+            throw error ;
+        }
+    }
+
+    async login({email , password}){
+        try {
+            return await this.account.createEmailSession(email , password);
+        } catch (error) {
+            throw error ;
+        }
+    }
+
+    async getCurrentUser({}){
+        try {
+            return await this.account.get() ;
+        } catch (error) {
+            throw error
+        }
+        return null ;
+    }
+
+    async logout({}){
+        try {
+            await this.account.deleteSessions() ; 
+        } catch (error) {
+            throw error ;
+        }
+    }
+}
+
+
+const authService = new AuthService();
+
+export default authService ;
